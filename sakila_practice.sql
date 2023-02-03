4c8c0222b1ac4920ed0fa153d4628565a7c74689
Vivian Do <vdo@sandiego.edu>
2023-02-03 15:21:45 -0800

    Update sakila_practice.sql

--- sakila_practice.sql
+++ sakila_practice.sql
@@ -78,9 +78,6 @@ select a.city_id, a.address, a2.address from address a
 INNER JOIN address a2
 on a.city_id= a2.city_id and a.address <> a2.address;
 
-
-
-
 #generate a list of all first and last names from multiple tables
 select 'cust' type, c.first_name, c.last_name from customer c
 UNION ALL
@@ -103,13 +100,99 @@ where a.first_name like 'J%' and a.last_name like 'D%';
 
 #Notes: The UNION operator allows the combination of multiple tables. UNION ALL keeps all duplicates. 
 
+#display the first and last names of all actors from the table actor
+select first_name, last_name from actor;
+
+#display the first and last name of each actor in a single column in upper case letters. Name the column 'Actor Name'
+select concat(first_name,' ',last_name) 'Actor Name' from actor;
+
+#You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
+select actor_id, first_name, last_name from actor
+where first_name = 'Joe';
+
+#Find all actors whose last name contain the letters GEN:
+select * from actor
+having last_name LIKE '%GEN%';
+
+#Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
+select * from actor
+having last_name LIKE '%LI%'
+order by last_name, first_name;
+
+#Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China
+select c.country_id, c.country from country c
+where c.country in ('Afghanistan', 'Bangladesh','China');
+
+#List the last names of actors, as well as how many actors have that last name.
+select last_name, count(last_name) from actor
+group by last_name;
+
+#List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+select last_name, count(last_name) from actor
+group by last_name
+having count(last_name) >1
+order by count(last_name) desc;
+
+#The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
+select * from actor
+where first_name = 'GROUCHO'; #find 'HARPO WILLIAMS' actor id (172)
+
+update actor
+set first_name = 'HARPO', last_name = 'WILLIAMS'
+where actor_id = '172'; #perform update
+
+select * from actor
+where actor_id='172'; #confirm update
+
+#Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! 
+	#In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
+update actor
+set first_name = 'GROUCHO'
+where first_name='HARPO' and last_name='WILLIAMS';
+select * from actor
+where actor_id='172'; #confirm update
+
+#Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
+select s.first_name, s.last_name, a.address, a.address2 from staff s
+inner join address a;
+
+#Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+select s.staff_id, s.first_name, s.last_name, sum(p.amount) from staff s
+join payment p
+on s.staff_id = p.staff_id
+where month(p.payment_date)=08 and year(p.payment_date)=2005
+group by s.staff_id;
+
+#List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+select f.title, count(*) from film f
+inner join film_actor fa
+on f.film_id = fa.film_id
+group by f.film_id
+order by count(*) desc;
+
+#How many copies of the film Hunchback Impossible exist in the inventory system?
 
 
+#Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
 
+#The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
 
+#Use subqueries to display all actors who appear in the film Alone Trip.
 
+#You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
 
+#Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
 
+#Display the most frequently rented movies in descending order.
 
+#Write a query to display how much business, in dollars, each store brought in.
 
+#Write a query to display for each store its store ID, city, and country.
 
+#List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+
+#In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
+
+#How would you display the view that you created in 8a?
+
+#You find that you no longer need the view top_five_genres. Write a query to delete it.
